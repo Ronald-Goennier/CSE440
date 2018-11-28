a0e579bb545a516806e8f6b42ea4a0a19ff6ba2c
Phillip Pascual <43511392+PhilPascual-CSUSB@users.noreply.github.com>
2018-11-27 17:51:18 -0800

    Add files via upload
    Phil's Scripts

---
+++ Phil's Jerpa Files/HealthKitMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//script to govern Health Kit movement
+
+public class HealthKitMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+    Rigidbody2D m_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        Physics2D.IgnoreLayerCollision(0, 8);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += Vector3.left * movementSpeed * Time.deltaTime;   	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(gameObject);
+    }
+}
