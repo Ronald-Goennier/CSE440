ee27e9cf3d63a0aafdeb1e51392c52e05fabf547
Phillip Pascual <43511392+PhilPascual-CSUSB@users.noreply.github.com>
2018-12-04 14:09:21 -0800

    Add files via upload
    Push Back and Stun scripts for the boss.

---
+++ Phil's Jerpa Files/PushBack Stuff/pushBack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* 
+ * Spawns the push back sphere upon collision.
+ */
+public class pushBack : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pushBackObject;
+
+    [SerializeField]
+    private Transform pushBackPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Instantiate(pushBackObject, pushBackPoint.position, pushBackPoint.rotation);
+        }
+    }
+}
---
+++ Phil's Jerpa Files/PushBack Stuff/pushBackExpansion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Expands the push back circle.
+ */
+
+public class pushBackExpansion : MonoBehaviour {
+
+    [SerializeField]
+    private float expansionTime;
+    private float expansionSpeed = 0;
+
+    //Update is called once per frame
+	void Update () {
+	    if (expansionSpeed < expansionTime/10.0f)
+        {
+            expansionSpeed += Time.deltaTime;
+            transform.localScale += new Vector3(expansionSpeed, expansionSpeed, expansionSpeed);
+        }
+        else
+        {
+            Destroy(gameObject, 1.0f);
+        }
+    }
+}
